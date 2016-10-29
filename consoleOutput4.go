66f81f6ce7cd3744178b069be5b84841f6328c13
aminagrebi <amin.agrebi2012@gmail.com>
2016-10-29 14:47:09 +0100

    Add files via upload

---
+++ consoleOutput4.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
+	isTrue := true
+
+	stringLengnth, err := fmt.Println(str1,str2,str3)
+
+    if err == nil {
+		fmt.Println("String Length:", stringLengnth)
+
+
+	}
+
+    fmt.Printf("Value of aNumber: %v\n", aNumber)
+    fmt.Printf("Value of isTrue: %v\n", isTrue)	
+    fmt.Printf("Value of aNumber as float: %.2f\n", float64(aNumber))
+
+    fmt.Printf("Data types: %T, %T, %T, %T, and %T",
+       str1,str2,str3,aNumber,isTrue)
+
+}
