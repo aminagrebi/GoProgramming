423efcdbb240d2b8180d5851c5755be46bfe5471
aminagrebi <amin.agrebi2012@gmail.com>
2016-10-29 14:15:56 +0100

    Update consoleOutput2.go

--- consoleOutput2.go
+++ consoleOutput2.go
@@ -7,8 +7,7 @@ func main() {
 	str1 := "The quick red fox"
 	str2 := "jumped over"
 	str3 := "the lazy brown dog."
-	//aNumber := 42
-	//isTrue := isTrue
+	
 
 	stringLengnth, err := fmt.Println(str1,str2,str3)
 
